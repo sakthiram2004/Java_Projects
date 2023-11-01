fdd4588640fc4c45d986b55ccda01bfa0cecab5d
sakthiram2004 <138390991+sakthiram2004@users.noreply.github.com>
2023-11-01 21:37:41 +0530

    object as argument

---
+++ complex_addition_or_object_as_argument.java
@@ -0,0 +1,42 @@
+import java.io.*;
+class Complex
+{
+int rp,ip;
+void input()throws IOException
+{
+BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+System.out.println("Enter rp,ip");
+rp=Integer.parseInt(br.readLine());
+ip=Integer.parseInt(br.readLine());
+}
+
+void process(Complex a,Complex b)
+{
+
+rp=a.rp+b.rp;
+ip=a.ip+b.ip;
+
+}
+void output()
+{
+if(ip>=0)
+System.out.println(rp+"+"+ip+"i");
+else
+System.out.println(rp+ip+"i");
+}
+}
+
+
+class Mymain1
+{
+public static void main(String arg[])throws IOException
+{
+Complex a=new Complex();
+Complex b=new Complex();
+Complex c=new Complex();
+a.input();
+b.input();
+c.process(a,b);
+c.output();
+}
+}
