45a33c6f3e5f44dc0011416048743a8cb2ece611
sakthiram2004 <138390991+sakthiram2004@users.noreply.github.com>
2023-11-08 21:22:09 +0530

    self_refferential_object

---
+++ self_refferencial.java
@@ -0,0 +1,18 @@
+import java.io.*;
+class Exam
+{
+void disp()
+{
+System.out.println("First disp");
+}
+void disp2()
+{
+System.out.println("second disp");
+}
+public static void main(String arg[])
+{
+Exam e=new Exam();
+e.disp();
+e.disp2();
+}
+}
